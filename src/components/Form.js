a52e63bc637a88aa64e61e0afcaab423c6eded56
karanwadhwa <wadhwa.karan96@gmail.com>
2019-04-17 17:01:48 +0530

    basic question structure in Form & MCQ components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 
 import MCQ from "./MCQ";
+import Result from "./Result";
+import questions from "../questions";
 
 class Form extends Component {
+  state = {
+    currentQuestion: questions[0],
+    selectedOption: "",
+    answeredQuestions: [],
+    upcomingQuestions: questions
+  };
+
+  selectOption = selectedOption => this.setState({ selectedOption });
+
   render() {
+    const {
+      currentQuestion,
+      selectedOption,
+      answeredQuestions,
+      upcomingQuestions
+    } = this.state;
+
     return (
       <div className="form-container">
-        <MCQ />
+        <MCQ
+          currentQuestion={currentQuestion}
+          selectedOption={selectedOption}
+          answeredQuestions={answeredQuestions}
+          upcomingQuestions={upcomingQuestions}
+          selectOption={this.selectOption}
+        />
       </div>
     );
   }
--- src/components/MCQ.js
+++ src/components/MCQ.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
 class MCQ extends Component {
+  renderOptions = () => {
+    const { currentQuestion, selectedOption } = this.props;
+    return currentQuestion.options.map(option => (
+      <div className="custom-control custom-radio" key={option.id}>
+        <input
+          type="radio"
+          id={`option${option.id}`}
+          name={`question${currentQuestion.id}`}
+          value={option.value}
+          onChange={e => this.props.selectOption(e.target.value)}
+          className="custom-control-input"
+          checked={option.value === selectedOption}
+        />
+        <label className="custom-control-label" htmlFor={`option${option.id}`}>
+          {option.value}
+        </label>
+      </div>
+    ));
+  };
+
   render() {
+    const { currentQuestion } = this.props;
     return (
       <div className="mcq">
-        <p>MCQ</p>
+        <h5>{currentQuestion.question}</h5>
+        <hr />
+        <div>{this.renderOptions()}</div>
       </div>
     );
   }
