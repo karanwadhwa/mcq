07f5ee4fe887c4cb117874c56a0f1344c4e3358a
karanwadhwa <wadhwa.karan96@gmail.com>
2019-04-17 16:27:33 +0530

    empty Form & MCQ component

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import MCQ from "./MCQ";
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="form-container">
+        <MCQ />
+      </div>
+    );
+  }
+}
+
+export default Form;
---
+++ src/components/MCQ.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MCQ extends Component {
+  render() {
+    return (
+      <div className="mcq">
+        <p>MCQ</p>
+      </div>
+    );
+  }
+}
+
+export default MCQ;
